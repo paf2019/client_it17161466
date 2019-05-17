e8c1f96e2a0e16bd264947be68c090ee23f8c699
“chanodhidesilva” <jeevanidesilva93@gmail.com>
2019-05-17 17:37:16 +0530

    third commit

--- demoClient/src/main/java/com/it17161466/client/clientImp/RestClient.java
+++ demoClient/src/main/java/com/it17161466/client/clientImp/RestClient.java
@@ -4,19 +4,34 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+//import com.it17161466.supplier.model.Items;
 import com.it17161466.supplier.model.User;
 
 @Component
 public class RestClient implements CommandLineRunner {
 
-	private static void callRestService() {
+	/*private static void callRestService1() {
 		RestTemplate restTemplate = new RestTemplate();
-		User user = restTemplate.getForObject("http://localhost:8089/api/suppliers/supplier/1", User.class);
+		User user = restTemplate.getForObject("http://localhost:8091/api/login", User.class);
 		System.out.println("User is :"+user.getName());
 	}
+	*/
+	private static void callRestService2() {
+		RestTemplate restTemplate = new RestTemplate();
+		User user = restTemplate.getForObject("http://localhost:8092/api/suppliers/supplier/1", User.class);
+		System.out.println("User is :"+user.getName());
+	}
+	
+	/*private static void callRestService3() {
+		RestTemplate restTemplate = new RestTemplate();
+		Items item = restTemplate.getForObject("http://localhost:8093/api/items/item/1", Items.class);
+		System.out.println("User is :"+item.getItem_name());
+	}*/
 
 	@Override
 	public void run(String... args) throws Exception {
-		callRestService();		
+		//callRestService1();	
+		callRestService2();	
+		//callRestService3();	
 	}
 }
